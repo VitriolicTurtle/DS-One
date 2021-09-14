79045fda4e6d717f88b379e84184b290933b5692
Marius <mma212@uit.no>
2021-09-14 19:03:12 +0200

    Server processing, ground structure for server threads. Ground structure for cache.
    Started Implementing Queries.

--- src/main/java/Server/ProxyServer.java
+++ src/main/java/Server/ProxyServer.java
@@ -17,8 +17,7 @@ public class ProxyServer extends Thread implements ProxyServerInterface{
     private int numServers;
 
     private ServerInterface[] servers;
-    private int[] serverPorts;
-    private int[] serverQueues;
+    private int[] serverQueuesSizes;
 
     /**
      * Constructor for the proxy-server.
@@ -28,10 +27,9 @@ public class ProxyServer extends Thread implements ProxyServerInterface{
         this.numServers = numServers;
         this.port = port;
 
-        // Set up arrays to store the server references, the servers' port numbers and the servers' workloads
+        // Set up arrays to store the server references and the servers' workloads
         this.servers = new ServerInterface[numServers];
-        this.serverPorts = new int[numServers];
-        this.serverQueues = new int[numServers];
+        this.serverQueuesSizes = new int[numServers];
 
         startProxyServer(registry);
     }
@@ -70,6 +68,7 @@ public class ProxyServer extends Thread implements ProxyServerInterface{
      */
     public void updateQueueData(int zone) throws RemoteException {
         int queueSize = servers[zone].getQueueSize();
+        serverQueuesSizes[zone] = queueSize;
     }
 
     /**
@@ -84,39 +83,34 @@ public class ProxyServer extends Thread implements ProxyServerInterface{
             System.out.println("\nError:\nInvalid zone number: " + zone + ".");
             System.exit(1);
         }
-        //TODO: Code cleanup?
 
         // Refer client to closest geographically located server if it has capacity
-        if (serverQueues[zone] < 10) {
-            return new ServerInfo("server_" + zone, serverPorts[zone]);
+        if (serverQueuesSizes[zone] < 10) {
+            return new ServerAddress("server_" + zone);
         }
 
         // If we are referring the client to one of the geographically neighboring servers
-        int neighborServer1 = zone - 1;
-        int neighborServer2 = zone + 1;
-
-        if (neighborServer1 == 6) { neighborServer1 = 1; }
-        else if (neighborServer1 == 0) { neighborServer1 = 5; }
-        if (neighborServer2 == 6) { neighborServer2 = 1; }
-        else if (neighborServer2 == 0) { neighborServer2 = 5; }
+        int neighborServer1 = (zone - 1) % numServers;
+        int neighborServer2 = (zone + 1) % numServers;
+        int selectedServer;
 
         // If both of the neighboring servers are also at maximum capacity, we refer the user to the
         // closest server
-        if (serverQueues[neighborServer1] >= 10 && serverQueues[neighborServer2] >= 10) {
-            return new ServerInfo("server_" + zone, serverPorts[zone]);
+        if (serverQueuesSizes[neighborServer1] >= 10 && serverQueuesSizes[neighborServer2] >= 10) {
+            selectedServer = zone;
         }
 
-        if (serverQueues[neighborServer1] == serverQueues[neighborServer2]) {
-            if (random.nextBoolean()) {
-                return new ServerInfo("server_" + neighborServer1, serverPorts[neighborServer1]);
-            } else {
-                return new ServerInfo("server_" + neighborServer2, serverPorts[neighborServer2]);
-            }
+        // If both the neighboring servers have equal workloads to each other, we choose one of them at random
+        if (serverQueuesSizes[neighborServer1] == serverQueuesSizes[neighborServer2]) {
+            selectedServer = (random.nextBoolean()) ? neighborServer1 : neighborServer2;
         }
-        if (serverQueues[neighborServer1] < serverQueues[neighborServer2]) {
-            return new ServerInfo("server_" + neighborServer1, serverPorts[neighborServer1]);
+
+        // Otherwise we choose the neighboring server with the lowest workload
+        //selectedServer =
+        if (serverQueuesSizes[neighborServer1] < serverQueuesSizes[neighborServer2]) {
+            return new ServerAddress("server_" + neighborServer1);
         } else {
-            return new ServerInfo("server_" + neighborServer2, serverPorts[neighborServer2]);
+            return new ServerAddress("server_" + neighborServer2);
         }
 
     }
--- src/main/java/Shared/ServerAddress.java
+++ src/main/java/Shared/ServerAddress.java
@@ -4,17 +4,19 @@ import java.io.Serializable;
 
 public class ServerAddress implements Serializable {
     public String address = null;
-    public int port = -1;
 
     /**
-     * Stores an address and port for a server.
+     * Stores an address for a server.
      * This is used to send the necessary data from the proxy-server to clients describing which server -
      * the client should connect to.
      * @param address: the server address name.
-     * @param port: the port the server is running on.
      */
-    public ServerInfo(String address, int port) {
+    public ServerAddress(String address) {
         this.address = address;
-        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        return address;
     }
 }
