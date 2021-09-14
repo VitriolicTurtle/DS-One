9d3e9ffdaf91f42fcdcdda1db450ee793bb96ecc
Marius <mma212@uit.no>
2021-09-14 19:03:17 +0200

    Server processing, ground structure for server threads. Ground structure for cache.
    Started Implementing Queries.

--- src/main/java/Shared/GetTopThreeMusicByUserQuery.java
+++ src/main/java/Shared/GetTopThreeMusicByUserQuery.java
@@ -3,6 +3,9 @@ package Shared;
 import java.io.File;
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
 
 /**
  * Class that gives the top 3 musicIDs a users has listened to based on the dataset.csv.
@@ -39,13 +42,23 @@ public class GetTopThreeMusicByUserQuery extends Query {
                 while (!data[userIndex].startsWith("U")) {                     // If there are more artists than 1, loop through indexes to find user.
                     userIndex++;
                 }
-                if(topSongsMap.containsKey(data[0])){
+                if(topSongsMap.containsKey(data[0])){                          // If song is already in the Hashmap, add times listened to keys value.
                     topSongsMap.put(data[0], topSongsMap.get(data[0]) + Integer.parseInt(data[userIndex+1]));
-                } else {
+                } else {                                                       //  Else, add the new key and its value.
                     topSongsMap.put(data[0], Integer.parseInt(data[userIndex+1]));
                 }
 
             }
+        }
+        Object[] topSongsArray = topSongsMap.entrySet().toArray();              // Array object created to sort HashMap by value.
+        Arrays.sort(topSongsArray, new Comparator(){                            // Sorts the array based on custom comparator function.
+            public int compare(Object val1, Object val2){
+                return((Map.Entry<String, Integer>) val2).getValue().compareTo(((Map.Entry<String, Integer>) val1).getValue());
+            }
+        });
+
+        for(Object s : topSongsArray){
+            System.out.println(((Map.Entry<String, Integer>) s).getKey() + " - Played: " + ((Map.Entry<String, Integer>) s).getValue());
         }
     }
 
